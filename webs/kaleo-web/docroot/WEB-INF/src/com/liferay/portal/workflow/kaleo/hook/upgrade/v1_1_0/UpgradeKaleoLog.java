e7e2e240ed8c6864b5407ed4bd17d97a90eb4228
Jang Kim <jang.kim@liferay.com>
2012-08-24 09:33:19 -0700

    LPS-29471 Kaelo Workflow tasks fail following upgrade from 6.0.12 to 6.1.10

---
+++ webs/kaleo-web/docroot/WEB-INF/src/com/liferay/portal/workflow/kaleo/hook/upgrade/v1_1_0/UpgradeKaleoLog.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.hook.upgrade.v1_1_0;
+
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.workflow.kaleo.util.WorkflowContextUtil;
+
+import java.io.Serializable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.Map;
+
+import org.jabsorb.JSONSerializer;
+
+/**
+ * @author Janghyun Kim
+ */
+public class UpgradeKaleoLog extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		JSONSerializer jsonSerializer = new JSONSerializer();
+
+		jsonSerializer.registerDefaultSerializers();
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+					"select kaleoLogId, workflowContext from KaleoLog where " +
+						"workflowContext is not null or workflowContext !='' " +
+							"and workflowContext not like '%serializable%'");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long kaleoLogId = rs.getLong("kaleoLogId");
+				String workflowContext = rs.getString("workflowContext");
+
+				String newWorkflowContext = WorkflowContextUtil.convert(
+					(Map<String, Serializable>)jsonSerializer.fromJSON(
+						workflowContext));
+
+				upgradeWorkflowContext(newWorkflowContext, kaleoLogId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void upgradeWorkflowContext(
+			String workflowContext, long kaleoLogId)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+				"update kaleoLog set workflowContext = ? where kaleoLogId = ?");
+
+			ps.setString(1, workflowContext);
+			ps.setLong(2, kaleoLogId);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps);
+		}
+	}
+
+}
