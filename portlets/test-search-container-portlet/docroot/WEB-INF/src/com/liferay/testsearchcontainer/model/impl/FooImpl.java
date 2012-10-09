6149b31f62cc9670882a0b978904d468407938e2
jjangsam <jang.kim@liferay.com>
2012-10-09 15:51:31 -0700

    LPS-30398 test-search-container-portlet - Source formatting (Done by hugo)

--- portlets/test-search-container-portlet/docroot/WEB-INF/src/com/liferay/testsearchcontainer/model/impl/FooImpl.java
+++ portlets/test-search-container-portlet/docroot/WEB-INF/src/com/liferay/testsearchcontainer/model/impl/FooImpl.java
@@ -15,20 +15,11 @@
 package com.liferay.testsearchcontainer.model.impl;
 
 /**
- * The extended model implementation for the Foo service. Represents a row in the &quot;TestSearchContainer_Foo&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.testsearchcontainer.model.Foo} interface.
- * </p>
- *
  * @author Janghyun Kim
  */
 public class FooImpl extends FooBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a foo model instance should use the {@link com.liferay.testsearchcontainer.model.Foo} interface instead.
-	 */
+
 	public FooImpl() {
 	}
+
 }
